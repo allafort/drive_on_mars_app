a63216385a0146b6b909ca707c1131bb86dc6058
Alice Allafort <allafort.alice@gmail.com>
2023-12-11 23:35:49 +0100

    Adding template with image loaded, call to API and background image

---
+++ app.py
@@ -0,0 +1,94 @@
+import streamlit as st
+from PIL import Image
+
+import requests
+
+# ----------------------------------------------------------------
+# Using local API
+url = 'http://127.0.0.1:8000'
+
+# Using our remote running API
+# url = 'https://containermars-jnjlxgdooq-ew.a.run.app/predict'
+#
+# ----------------------------------------------------------------
+
+
+# Set the background image
+background_image = """
+<style>
+[data-testid="stAppViewContainer"] > .main {
+    background-image: url("https://caltech-prod.s3.amazonaws.com/main/images/Mars_Mastcam_1.2e16d0ba.fill-1600x810-c100.jpg");
+    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+    background-position: center;
+    background-repeat: no-repeat;
+}
+</style>
+"""
+
+st.markdown(background_image, unsafe_allow_html=True)
+
+
+# ----------------------------------------------------------------
+# App title and description
+
+st.title('Drive on Mars ')
+st.markdown('''
+            > This is our project...
+
+
+            > * [Our Github repo](https://github.com/TigerManon/drive-on-mars)
+
+            ''')
+
+st.markdown("---")
+
+
+### Create a native Streamlit file upload input
+st.markdown("### Let's do a simple image upload 👇")
+img_file_buffer = st.file_uploader('Upload an image')
+
+
+if img_file_buffer is not None:
+
+  col1, col2 = st.columns(2)
+
+  with col1:
+    ### Display the image user uploaded
+    st.image(Image.open(img_file_buffer), caption="Mars Curiosity Image")
+
+  with col2:
+    with st.spinner("Wait for it..."):
+      ### Get bytes from the file buffer
+      img_bytes = img_file_buffer.getvalue()
+
+      ### Make request to  API (stream=True to stream response as bytes)
+      res = requests.post(url + "/upload_image", files={'img': img_bytes})
+
+      if res.status_code == 200:
+        ### Display the image returned by the API
+        st.image(res.content, caption="Reconstructed landscape")
+      else:
+        st.markdown("**Oops**, something went wrong 😓 Please try again.")
+        print(res.status_code, res.content)
+
+
+
+st.markdown("---"*10)
+
+st.markdown('''
+            > Additional information
+
+            ''')
+
+param1 = st.slider('Select a number', 1, 10, 3)
+
+param2 = st.slider('Select another number', 1, 10, 3)
+
+
+params = {
+    'feature1': param1,  # 0 for Sunday, 1 for Monday, ...
+    'feature2': param2
+}
+response = requests.get(url + "/predict", params=params)
+
+st.text(response.json())
