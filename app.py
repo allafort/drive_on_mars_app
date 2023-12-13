685095460c994bfb0d938601f0b7860a82667b21
Alice Allafort <allafort.alice@gmail.com>
2023-12-13 16:05:13 +0100

    Add image gallery

--- app.py
+++ app.py
@@ -151,4 +151,96 @@ with upload_tab:
                 st.markdown("**Oops**, something went wrong 😓 Please try again.")
 
 with gallery_tab:
-    pass
+
+    options = list(gallery_dict.keys())
+    file_name = st.selectbox("Select Mars Terrain",
+                            options=options, index=options.index("Bedrock Ridge"))
+    file = gallery_dict[file_name]
+
+    # if st.session_state.get("file_uploader") is not None:
+    #     st.warning("To use the Gallery, remove the uploaded image first.")
+    # if st.session_state.get("image_url") not in ["", None]:
+    #     st.warning("To use the Gallery, remove the image URL first.")
+
+    img = Image.open(file)
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        ### Display the image user uploaded
+        st.image(img, caption="Mars Curiosity Image")
+
+    with col2:
+        with st.spinner("Landscape is being analyzed..."):
+            ### Get bytes from the file buffer
+            with open(file,'rb') as f:
+                img_bytes = f.read()
+
+            ### Make request to  API (stream=True to stream response as bytes)
+            res = requests.post(url + "/upload_image", files={'img': img_bytes})
+
+        ## When API returns image
+        # if res.status_code == 200:
+        #     ### Display the image returned by the API
+        #     st.image(res.content, caption="Reconstructed landscape")
+        # else:
+        #     st.markdown("**Oops**, something went wrong 😓 Please try again.")
+        #     print(res.status_code, res.content)
+
+        ### When API returns numpy array
+        if res.status_code == 200:
+            y_pred = res.content
+            y_pred_arr = np.frombuffer(y_pred, np.uint8).reshape((256, 256))
+            y_pred_arr = y_pred_arr.repeat(4, axis=0).repeat(4, axis=1)
+
+            # Plotting
+            colors=[
+                (0, 0.5, 1), # soil
+                (0, 1, 0.5), # bed rocks
+                (1, 1, 0), # sand
+                (1, 0, 0), # big rocks
+                (0, 0, 0)] # null
+
+            # colors = [(tup[0]*255,tup[1]*255,tup[2]*255,) for tup in colors]
+            # print(colors)
+
+            # image_back = np.array(Image.open(img_file_buffer).resize((256, 256)))
+            image_back = np.array(Image.open(file))
+
+            print(y_pred_arr.shape, image_back.shape)
+
+            bg_color = (0, 0, 0, 0.1)
+            output_image = color.label2rgb(
+                y_pred_arr,
+                alpha = 0.3,
+                image = image_back,
+                colors = colors,
+                bg_label = 4,
+                bg_color = bg_color
+                )
+
+            st.image(output_image, caption="Reconstructed landscape")
+
+
+            # # With matplotlib
+            # fig, ax = plt.subplots()
+
+            # ax.imshow(output_image)
+
+            # # # Création de la légende
+            # legend_labels = ['Soil', 'Bed Rocks', 'Sand', 'Big Rocks', 'Other']
+            # legend_colors = [(0, 0.5, 1, 0.5), (0.0, 1.0, 0.5, 0.5), (1, 1, 0, 0.5), (1.0, 0.0, 0.0, 0.5), (0.0, 0.0, 0.0, 0.1)]
+            # # Création des patches (carrés colorés) pour la légende
+            # patches = [ax.plot([], [], marker='s', markersize=10, linestyle='', color=color)[0] for color in legend_colors]
+
+            # # Ajout de la légende au plot
+            # ax.legend(patches, legend_labels, loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=len(legend_labels), handlelength=0.5, handletextpad=0.5)
+
+            # ax.axis('off')
+
+            # st.pyplot(fig)
+
+
+
+        else:
+            st.markdown("**Oops**, something went wrong 😓 Please try again.")
