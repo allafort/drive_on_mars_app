6114bffd15b48dcb8fc42a6108e42d03b7aca84b
Alice Allafort <allafort.alice@gmail.com>
2023-12-13 12:17:50 +0100

    Now using the app on front end to plot the prediction provided by the API as a numpy array. Using scikit image package

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from PIL import Image
 
 import requests
+from skimage import color
+import numpy as np
+# import matplotlib.pyplot as plt
 
 # ----------------------------------------------------------------
 # Using local API
@@ -34,13 +37,8 @@ st.markdown(background_image, unsafe_allow_html=True)
 st.title('Drive on Mars ')
 st.markdown('''
             > This is our project...
-
-
-            > * [Our Github repo](https://github.com/TigerManon/drive-on-mars)
-
+            > [Our Github repo](https://github.com/TigerManon/drive-on-mars)
             ''')
-
-st.markdown("---")
 
 
 ### Create a native Streamlit file upload input
@@ -57,16 +55,75 @@ if img_file_buffer is not None:
     st.image(Image.open(img_file_buffer), caption="Mars Curiosity Image")
 
   with col2:
-    with st.spinner("Wait for it..."):
+    with st.spinner("Landscape is being analyzed..."):
       ### Get bytes from the file buffer
       img_bytes = img_file_buffer.getvalue()
 
       ### Make request to  API (stream=True to stream response as bytes)
       res = requests.post(url + "/upload_image", files={'img': img_bytes})
 
-      if res.status_code == 200:
-        ### Display the image returned by the API
-        st.image(res.content, caption="Reconstructed landscape")
-      else:
+    ### When API returns image
+    # if res.status_code == 200:
+    #     ### Display the image returned by the API
+    #     st.image(res.content, caption="Reconstructed landscape")
+    # else:
+    #     st.markdown("**Oops**, something went wrong 😓 Please try again.")
+    #     print(res.status_code, res.content)
+
+    ### When API returns numpy array
+    if res.status_code == 200:
+        y_pred = res.content
+        y_pred_arr = np.frombuffer(y_pred, np.uint8).reshape((256, 256))
+        y_pred_arr = y_pred_arr.repeat(4, axis=0).repeat(4, axis=1)
+
+        # Plotting
+        colors=[
+            (0, 0.5, 1), # soil
+            (0, 1, 0.5), # bed rocks
+            (1, 1, 0), # sand
+            (1, 0, 0), # big rocks
+            (0, 0, 0)] # null
+
+        # colors = [(tup[0]*255,tup[1]*255,tup[2]*255,) for tup in colors]
+        # print(colors)
+
+        # image_back = np.array(Image.open(img_file_buffer).resize((256, 256)))
+        image_back = np.array(Image.open(img_file_buffer))
+
+        print(y_pred_arr.shape, image_back.shape)
+
+        bg_color = (0, 0, 0, 0.1)
+        output_image = color.label2rgb(
+            y_pred_arr,
+            alpha = 0.3,
+            image = image_back,
+            colors = colors,
+            bg_label = 4,
+            bg_color = bg_color
+            )
+
+        st.image(output_image, caption="Reconstructed landscape")
+
+
+        # # With matplotlib
+        # fig, ax = plt.subplots()
+
+        # ax.imshow(output_image)
+
+        # # # Création de la légende
+        # legend_labels = ['Soil', 'Bed Rocks', 'Sand', 'Big Rocks', 'Other']
+        # legend_colors = [(0, 0.5, 1, 0.5), (0.0, 1.0, 0.5, 0.5), (1, 1, 0, 0.5), (1.0, 0.0, 0.0, 0.5), (0.0, 0.0, 0.0, 0.1)]
+        # # Création des patches (carrés colorés) pour la légende
+        # patches = [ax.plot([], [], marker='s', markersize=10, linestyle='', color=color)[0] for color in legend_colors]
+
+        # # Ajout de la légende au plot
+        # ax.legend(patches, legend_labels, loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=len(legend_labels), handlelength=0.5, handletextpad=0.5)
+
+        # ax.axis('off')
+
+        # st.pyplot(fig)
+
+
+
+    else:
         st.markdown("**Oops**, something went wrong 😓 Please try again.")
-        print(res.status_code, res.content)
